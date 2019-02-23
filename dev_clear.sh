c3e5647f94a4996e4801c4cd970c8a1e56ca3559
Nick Bisby <nbisby@gmail.com>
2019-02-22 21:14:56 -0600

    Add roku upload scripts

---
+++ dev_clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$ROKU_DEV_PASSWORD" ]; then
+    echo "You need to set ROKU_DEV_PASSWORD:"
+    echo "export ROKU_DEV_PASSWORD=<your dev password>"
+    exit 1
+fi
+if [ -z "$ROKU_DEV_TARGET" ]; then
+    echo "You need to set ROKU_DEV_TARGET:"
+    echo "export ROKU_DEV_TARGET=<your roku ip>"
+    exit 1
+fi
+
+curl -f -sS --user rokudev:$ROKU_DEV_PASSWORD --anyauth -F "mysubmit=Delete" -F"archive=" http://$ROKU_DEV_TARGET/plugin_install  \
+    | python -c 'import sys, re; print("\n".join(re.findall("<font color=\"red\">(.*?)</font>", sys.stdin.read(), re.DOTALL)))'
