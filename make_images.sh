43fe16395bea59bec3dc12ff86a2afcd77692cef
Nick Bisby <nbisby@gmail.com>
2019-04-29 11:11:45 -0500

    Resize poster images and commit images

--- make_images.sh
+++ make_images.sh
@@ -16,9 +16,9 @@ if [ ! -e $ICON_SOURCE ]; then
 fi
 
 # Channel Posters
-convert -background "#000b25" -gravity center -scale 400x400 -extent 540x405 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_fhd.png
-convert -background "#000b25" -gravity center -scale 360x360 -extent 486x365 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_hd.png
-convert -background "#000b25" -gravity center -scale 320x320 -extent 432x324 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_sd.png
+convert -background "#000b25" -gravity center -scale 535x400 -extent 540x405 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_fhd.png
+convert -background "#000b25" -gravity center -scale 275x205 -extent 280x210 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_hd.png
+convert -background "#000b25" -gravity center -scale 182x135 -extent 187x140 $BANNER_SOURCE $OUTPUT_DIR/channel-poster_sd.png
 
 # Overhang icon
 convert -background none -gravity center -scale 1000x48 -extent 180x48 $BANNER_SOURCE $OUTPUT_DIR/logo.png
