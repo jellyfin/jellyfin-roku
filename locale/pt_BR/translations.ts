d2ac8546165cf7a070969b94a8929f6e68adf4e2
Vitorvlv <vitorlourovaladares@gmail.com>
2020-05-25 13:00:58 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (42 of 42 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/pt_BR/

--- locale/pt_BR/translations.ts
+++ locale/pt_BR/translations.ts
@@ -1,217 +1,187 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_CA" sourcelanguage="en_US">
-    <defaultcodec>UTF-8</defaultcodec>
-    <context>
-        <name>default</name>
-        <message>
-            <source>192.168.1.100:8096 or https://example.com/jellyfin</source>
-            <translation>192.168.1.100:8096 ou https://example.com/jellyfin</translation>
-        </message>
-
-        <message>
-            <source>Cancel</source>
-            <translation>Cancelar</translation>
-        </message>
-
-        <message>
-            <source>Connect to Server</source>
-            <translation>Conectar ao Servidor</translation>
-        </message>
-
-        <message>
-            <source>Ends at %1</source>
-            <translation>Termina às %1</translation>
-        </message>
-
-        <message>
-            <source>Enter Configuration</source>
-            <translation>Digite Configuração</translation>
-        </message>
-
-        <message>
-            <source>Favorite</source>
-            <translation>Favorito</translation>
-        </message>
-
-        <message>
-            <source>Loading...</source>
-            <translation>Carregando...</translation>
-        </message>
-
-        <message>
-            <source>Login attempt failed.</source>
-            <translation>Falha na tentativa de login.</translation>
-        </message>
-
-        <message>
-            <source>OK</source>
-            <translation>OK</translation>
-        </message>
-
-        <message>
-            <source>Options</source>
-            <translation>Opções</translation>
-        </message>
-
-        <message>
-            <source>Play</source>
-            <translation>Reproduzir</translation>
-        </message>
-
-        <message>
-            <source>Please sign in</source>
-            <translation>Por favor, inicie a sessão</translation>
-        </message>
-
-        <message>
-            <source>Search</source>
-            <translation>Busca</translation>
-        </message>
-
-        <message>
-            <source>Server not found, is it online?</source>
-            <translation>SServidor não encontrado, ele está online?</translation>
-        </message>
-
-        <message>
-            <source>Shuffle</source>
-            <translation>Aleatório</translation>
-        </message>
-
-        <message>
-            <source>Sign In</source>
-            <translation>Entrar</translation>
-        </message>
-
-        <message>
-            <source>Submit</source>
-            <translation>Enviar</translation>
-        </message>
-
-        <message>
-            <source>Watched</source>
-            <translation>Assistido</translation>
-        </message>
-
-        <message>
-            <source>Change server</source>
-            <translation>Alterar Servidor</translation>
-        </message>
-
-        <message>
-            <source>Sign out</source>
-            <translation>Sair</translation>
-        </message>
-
-        <message>
-            <source>Add User</source>
-            <translation>Adicionar Usuário</translation>
-        </message>
-
-        <message>
-            <source>Profile</source>
-            <translation>Perfil</translation>
-        </message>
-
-        <message>
-            <source>My Media</source>
-            <translation>Minha Mídia</translation>
-        </message>
-
-        <message>
-            <source>Continue Watching</source>
-            <translation>Continuar assistindo</translation>
-        </message>
-
-        <message>
-            <source>Next Up</source>
-            <translation>A Seguir</translation>
-        </message>
-
-        <message>
-            <source>Latest in</source>
-            <translation>Latest in</translation>
-        </message>
-
-        <message>
-            <source>Home</source>
-            <translation>Inicio</translation>
-        </message>
-
-        <message>
-            <source>Enter a value...</source>
-            <translation>Digite um valor...</translation>
-        </message>
-
-        <message>
-            <source>Sort Field</source>
-            <translation>Ordenar Por</translation>
-        </message>
-
-        <message>
-            <source>Date Added</source>
-            <translation>Data de Adição</translation>
-        </message>
-
-        <message>
-            <source>Release Date</source>
-            <translation>Data de Lançamento</translation>
-        </message>
-
-        <message>
-            <source>Name</source>
-            <translation>Nome</translation>
-        </message>
-
-        <message>
-            <source>Sort Order</source>
-            <translation>Ordem</translation>
-        </message>
-
-        <message>
-            <source>Descending</source>
-            <translation>Decrescente</translation>
-        </message>
-
-        <message>
-            <source>Ascending</source>
-            <translation>Crescente</translation>
-        </message>
-
-        <message>
-            <source>Password</source>
-            <translation>Senha</translation>
-        </message>
-
-        <message>
-            <source>Username</source>
-            <translation>Usuário</translation>
-        </message>
-
-        <message>
-            <source>Genres</source>
-            <translation>Gêneros</translation>
-        </message>
-
-        <message>
-            <source>Director</source>
-            <translation>Diretor</translation>
-        </message>
-
-        <message>
-            <source>Video</source>
-            <translation>Vídeo</translation>
-        </message>
-
-        <message>
-            <source>Audio</source>
-            <translation>Áudio</translation>
-        </message>
-
-        <message>
-            <source>Server</source>
-            <translation>Servidor</translation>
-        </message>
-    </context>
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>default</name>
+    <message>
+        <source>192.168.1.100:8096 or https://example.com/jellyfin</source>
+        <translation>192.168.1.100:8096 ou https://example.com/jellyfin</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Connect to Server</source>
+        <translation>Conectar ao Servidor</translation>
+    </message>
+    <message>
+        <source>Ends at %1</source>
+        <translation>Termina às %1</translation>
+    </message>
+    <message>
+        <source>Enter Configuration</source>
+        <translation>Digite Configuração</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Favorito</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Carregando...</translation>
+    </message>
+    <message>
+        <source>Login attempt failed.</source>
+        <translation>Falha na tentativa de login.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opções</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Reproduzir</translation>
+    </message>
+    <message>
+        <source>Please sign in</source>
+        <translation>Por favor, inicie a sessão</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Busca</translation>
+    </message>
+    <message>
+        <source>Server not found, is it online?</source>
+        <translation>SServidor não encontrado, ele está online?</translation>
+    </message>
+    <message>
+        <source>Shuffle</source>
+        <translation>Aleatório</translation>
+    </message>
+    <message>
+        <source>Sign In</source>
+        <translation>Entrar</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation>Enviar</translation>
+    </message>
+    <message>
+        <source>Watched</source>
+        <translation>Assistido</translation>
+    </message>
+    <message>
+        <source>Change server</source>
+        <translation>Alterar Servidor</translation>
+    </message>
+    <message>
+        <source>Sign out</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <source>Add User</source>
+        <translation>Adicionar Usuário</translation>
+    </message>
+    <message>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <source>My Media</source>
+        <translation>Minha Mídia</translation>
+    </message>
+    <message>
+        <source>Continue Watching</source>
+        <translation>Continuar assistindo</translation>
+    </message>
+    <message>
+        <source>Next Up</source>
+        <translation>A Seguir</translation>
+    </message>
+    <message>
+        <source>Latest in</source>
+        <translation>Latest in</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Enter a value...</source>
+        <translation>Digite um valor...</translation>
+    </message>
+    <message>
+        <source>Sort Field</source>
+        <translation>Ordenar Por</translation>
+    </message>
+    <message>
+        <source>Date Added</source>
+        <translation>Data de Adição</translation>
+    </message>
+    <message>
+        <source>Release Date</source>
+        <translation>Data de Lançamento</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Sort Order</source>
+        <translation>Ordem</translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation>Decrescente</translation>
+    </message>
+    <message>
+        <source>Ascending</source>
+        <translation>Crescente</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Senha</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Usuário</translation>
+    </message>
+    <message>
+        <source>Genres</source>
+        <translation>Gêneros</translation>
+    </message>
+    <message>
+        <source>Director</source>
+        <translation>Diretor</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Vídeo</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>Áudio</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Servidor</translation>
+    </message>
+</context>
+<context>
+    <name></name>
+    <message>
+        <source>Sign Out</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <source>Change Server</source>
+        <translation>Mudar servidor</translation>
+    </message>
+</context>
 </TS>
