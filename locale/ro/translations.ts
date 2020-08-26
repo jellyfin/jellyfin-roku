fb01a52c4d8e898bf4df288360508cf965af4ff2
4d1m <silviu.dimulete@gmail.com>
2020-08-26 11:44:35 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (62 of 62 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/ro/

--- locale/ro/translations.ts
+++ locale/ro/translations.ts
@@ -224,5 +224,94 @@
         <translation>Eroare la preluarea conținutului</translation>
         <extracomment>Dialog title when unable to load Content from Server</extracomment>
     </message>
+    <message>
+        <comment>Title of Tab for options to filter library content</comment>
+        <source>TAB_FILTER</source>
+        <translation>Filtru</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to sort library content</comment>
+        <source>TAB_SORT</source>
+        <translation>Sortează</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for switching &quot;views&quot; when looking at a library</comment>
+        <source>TAB_VIEW</source>
+        <translation>Vedere</translation>
+    </message>
+    <message>
+        <source>RUNTIME</source>
+        <translation>Timp de rulare</translation>
+    </message>
+    <message>
+        <source>RELEASE_DATE</source>
+        <translation>Dată Lansare</translation>
+    </message>
+    <message>
+        <source>PLAY_COUNT</source>
+        <translation>Contorizare rulări</translation>
+    </message>
+    <message>
+        <source>OFFICIAL_RATING</source>
+        <translation>Limită de vârstă</translation>
+    </message>
+    <message>
+        <source>DATE_PLAYED</source>
+        <translation>Dată Rulare</translation>
+    </message>
+    <message>
+        <source>DATE_ADDED</source>
+        <translation>Dată Adăugare</translation>
+    </message>
+    <message>
+        <source>CRITIC_RATING</source>
+        <translation>Rating Critic</translation>
+    </message>
+    <message>
+        <source>IMDB_RATING</source>
+        <translation>Rating IMDb</translation>
+    </message>
+    <message>
+        <comment>Name or Title field of media item</comment>
+        <source>TITLE</source>
+        <translation>Nume</translation>
+    </message>
+    <message>
+        <comment>Message displayed in Item Grid when no item to display. %1 is container type (e.g. Boxset, Collection, Folder, etc)</comment>
+        <source>NO_ITEMS</source>
+        <translation>Acest %1 nu conține articole</translation>
+    </message>
+    <message>
+        <source>Unable to load Channel Data from the server</source>
+        <translation>Imposibil de încărcat datele canalului de pe server</translation>
+    </message>
+    <message>
+        <source>Error loading Channel Data</source>
+        <translation>Eroare la încărcarea datelor canalului</translation>
+    </message>
+    <message>
+        <source>Loading Channel Data</source>
+        <translation>Se încarcă datele canalului</translation>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item.</source>
+        <translation>A apărut o eroare la redarea acestui element.</translation>
+        <extracomment>Dialog detail when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>There was an error retrieving the data for this item from the server.</source>
+        <translation>A apărut o eroare la preluarea datelor de pe server pentru acest element.</translation>
+        <extracomment>Dialog detail when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>Error During Playback</source>
+        <translation>Eroare în timpul redării</translation>
+        <extracomment>Dialog title when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>Error Retrieving Content</source>
+        <translation>Eroare la recuperarea conținutului</translation>
+        <extracomment>Dialog title when unable to load Content from Server</extracomment>
+    </message>
 </context>
 </TS>
