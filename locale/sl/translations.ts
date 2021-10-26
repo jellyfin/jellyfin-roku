a0ef7e82ea41b4a02e743af4504a521fd0270ad4
Alan Azar <alanazar304@hotmail.com>
2021-10-26 15:40:39 +0000

    Translated using Weblate (Slovenian)
    Currently translated at 47.0% (40 of 85 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/sl/

--- locale/sl/translations.ts
+++ locale/sl/translations.ts
@@ -30,7 +30,7 @@
     </message>
     <message>
         <source>Loading...</source>
-        <translation>Nalaganje...</translation>
+        <translation>Nalaganje…</translation>
     </message>
     <message>
         <source>Login attempt failed.</source>
@@ -114,7 +114,7 @@
     </message>
     <message>
         <source>Enter a value...</source>
-        <translation>Vnesite vrednost...</translation>
+        <translation>Vnesite vrednost…</translation>
     </message>
     <message>
         <source>Sort Field</source>
