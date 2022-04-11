6492fd3eda5c2507d5765e88cacc60c112b3f8bc
Николай Иванов <nivanov@mail.bg>
2022-04-11 22:31:31 +0000

    Translated using Weblate (Bulgarian)
    Currently translated at 100.0% (106 of 106 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/bg/

--- locale/bg/translations.ts
+++ locale/bg/translations.ts
@@ -6,7 +6,7 @@
     <name>default</name>
     <message>
         <source>192.168.1.100:8096 or https://example.com/jellyfin</source>
-        <translation>192.168.1.100:8096 or https://пример.com/jellyfin</translation>
+        <translation>default192.168.1.100:8096 или https://пример.com/jellyfin</translation>
     </message>
     <message>
         <source>Cancel</source>
@@ -446,6 +446,283 @@
         <source>today</source>
         <translation>днес</translation>
         <extracomment>Current day</extracomment>
+    </message>
+    <message>
+        <comment>Message displayed in Item Grid when no item to display. %1 is container type (e.g. Boxset, Collection, Folder, etc)</comment>
+        <source>NO_ITEMS</source>
+        <translation>Този %1 не съдържа артикули</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Версия</translation>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item.  Server did not provide required transcoding data.</source>
+        <translation>Възникна грешка при възпроизвеждането на този файл.Сървърът не предоставя данни за транскодирането.</translation>
+        <extracomment>Content of message box when trying to play an item which requires transcoding, and the server did not provide transcode url</extracomment>
+    </message>
+    <message>
+        <source>Error Getting Playback Information</source>
+        <translation>Грешка при извличането на информация за възпроизвеждането</translation>
+        <extracomment>Dialog Title: Received error from server when trying to get information about the selected item for playback</extracomment>
+    </message>
+    <message>
+        <source>...or enter server URL manually:</source>
+        <translation>.. или въведете адреса ръчно:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to manually enter a server URL</extracomment>
+    </message>
+    <message>
+        <source>Pick a Jellyfin server from the local network</source>
+        <translation>Изберете Джелифин сървър от локалната мрежа</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to pick a server from a list</extracomment>
+    </message>
+    <message>
+        <source>Enter the server name or ip address</source>
+        <translation>Въведете име или ИП адрес на сървър</translation>
+        <extracomment>Title of KeyboardDialog when manually entering a server URL</extracomment>
+    </message>
+    <message>
+        <source>The requested content does not exist on the server</source>
+        <translation>Търсеното съдържание не е намерено на този сървър</translation>
+        <extracomment>Content of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Not found</source>
+        <translation>Не е намерен</translation>
+        <extracomment>Title of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Connecting to Server</source>
+        <translation>Свързване към сървър</translation>
+        <extracomment>Message to display to user while client is attempting to connect to the server</extracomment>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Затвори</translation>
+    </message>
+    <message>
+        <source>Cancel Series Recording</source>
+        <translation>Спри записа на сериал</translation>
+    </message>
+    <message>
+        <source>Cancel Recording</source>
+        <translation>Спри записа</translation>
+    </message>
+    <message>
+        <source>Record Series</source>
+        <translation>Запиши сериал</translation>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation>Запиши</translation>
+    </message>
+    <message>
+        <source>View Channel</source>
+        <translation>Виж канал</translation>
+    </message>
+    <message>
+        <source>TV Guide</source>
+        <translation>Тв Справочник</translation>
+        <extracomment>Menu option for showing Live TV Guide / Schedule</extracomment>
+    </message>
+    <message>
+        <source>Channels</source>
+        <translation>Канали</translation>
+        <extracomment>Menu option for showing Live TV Channel List</extracomment>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation>Повтори</translation>
+        <extracomment>If TV Shows has previously been broadcasted</extracomment>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>На живо</translation>
+        <extracomment>If TV Show is being broadcast live (not pre-recorded)</extracomment>
+    </message>
+    <message>
+        <source>Ends at</source>
+        <translation>Приключва в</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program ended  (e.g.  Ended Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ended at</source>
+        <translation>Приключва на</translation>
+        <extracomment>(Past Tense) For defining time when a program will ended (e.g.  Ended at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts</source>
+        <translation>Стартира</translation>
+        <extracomment>(Future Tense) For defining a day and time when a program will start (e.g.  Starts Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts at</source>
+        <translation>Стартира в</translation>
+        <extracomment>(Future Tense) For defining time when a program will start today (e.g.  Starts at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Started</source>
+        <translation>Стартира</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program started (e.g.  Started Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Started at</source>
+        <translation>Стартира на</translation>
+        <extracomment>(Past Tense) For defining time when a program started today (e.g.  Started at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation>Събота</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation>Петък</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation>Четвъртък</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation>Сряда</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation>Вторник</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation>Понеделник</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation>Неделя</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation>утре</translation>
+        <extracomment>Next day</extracomment>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation>вчера</translation>
+        <extracomment>Previous day</extracomment>
+    </message>
+    <message>
+        <source>today</source>
+        <translation>днес</translation>
+        <extracomment>Current day</extracomment>
+    </message>
+    <message>
+        <source>TV Shows</source>
+        <translation>Сериали</translation>
+    </message>
+    <message>
+        <source>Movies</source>
+        <translation>Филми</translation>
+    </message>
+    <message>
+        <source>Press &apos;OK&apos; to Close</source>
+        <translation>Натиснете &quot;Да&quot; за изход</translation>
+    </message>
+    <message>
+        <source>Special Features</source>
+        <translation>Допълнителни материали</translation>
+        <message>
+            <source>Press &apos;OK&apos; to Close</source>
+            <translation>Press &apos;OK&apos; to Close</translation>
+        </message>
+    </message>
+    <message>
+        <source>More Like This</source>
+        <translation>Подобни на тези</translation>
+    </message>
+    <message>
+        <source>Cast &amp; Crew</source>
+        <translation>Актьори и екип</translation>
+    </message>
+    <message>
+        <source>Age</source>
+        <translation>Възраст</translation>
+    </message>
+    <message>
+        <source>Died</source>
+        <translation>Починал</translation>
+    </message>
+    <message>
+        <source>Born</source>
+        <translation>Роден</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to filter library content</comment>
+        <source>TAB_FILTER</source>
+        <translation>Филтър</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to sort library content</comment>
+        <source>TAB_SORT</source>
+        <translation>Сортирай</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for switching &quot;views&quot; when looking at a library</comment>
+        <source>TAB_VIEW</source>
+        <translation>Изглед</translation>
+    </message>
+    <message>
+        <source>RUNTIME</source>
+        <translation>Време на възпроизвеждане</translation>
+    </message>
+    <message>
+        <source>RELEASE_DATE</source>
+        <translation>Дата на издаване</translation>
+    </message>
+    <message>
+        <source>PLAY_COUNT</source>
+        <translation>Брой пускания</translation>
+    </message>
+    <message>
+        <source>OFFICIAL_RATING</source>
+        <translation>Родителски рейтинг</translation>
+    </message>
+    <message>
+        <source>DATE_PLAYED</source>
+        <translation>Дата на възпроизвеждане</translation>
+    </message>
+    <message>
+        <source>DATE_ADDED</source>
+        <translation>Дата на добавяне</translation>
+    </message>
+    <message>
+        <source>CRITIC_RATING</source>
+        <translation>Рейтинг на критиката</translation>
+    </message>
+    <message>
+        <source>IMDB_RATING</source>
+        <translation>Рейтинг в IMDb</translation>
+    </message>
+    <message>
+        <comment>Name or Title field of media item</comment>
+        <source>TITLE</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <source>On Now</source>
+        <translation>Включено в момента</translation>
+    </message>
+    <message>
+        <source>Delete Saved</source>
+        <translation>Изтрий запазените</translation>
+    </message>
+    <message>
+        <source>Save Credentials?</source>
+        <translation>Да запазя ли данни за удостоверение?</translation>
     </message>
 </context>
 </TS>
