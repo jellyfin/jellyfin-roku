c728678c6d80830be266e066dc77128d18621843
Gleb Golov <gleb@golov.dev>
2022-06-05 15:02:25 +0000

    Translated using Weblate (Italian)
    Currently translated at 37.2% (48 of 129 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/it/

--- locale/it_IT/translations.ts
+++ locale/it_IT/translations.ts
@@ -1532,5 +1532,53 @@
         <source>Change Server</source>
         <translation>Cambia server</translation>
     </message>
+    <message>
+        <source>Error loading Channel Data</source>
+        <translation>Errore nel caricamento dei dati del canale</translation>
+    </message>
+    <message>
+        <source>Loading Channel Data</source>
+        <translation>Caricamento dati canale</translation>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item.</source>
+        <translation>C&apos;è stato un errore durante la riproduzione di questo oggetto.</translation>
+        <extracomment>Dialog detail when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>There was an error retrieving the data for this item from the server.</source>
+        <translation>C&apos;è stato un errore nel recupero dei dati per questo oggetto dal server.</translation>
+        <extracomment>Dialog detail when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>Error During Playback</source>
+        <translation>Errore nella riproduzione</translation>
+        <extracomment>Dialog title when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>Error Retrieving Content</source>
+        <translation>Errore recupero contenuti</translation>
+        <extracomment>Dialog title when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>On Now</source>
+        <translation>In onda ora</translation>
+    </message>
+    <message>
+        <source>Delete Saved</source>
+        <translation>Elimina salvati</translation>
+    </message>
+    <message>
+        <source>Save Credentials?</source>
+        <translation>Salvare le credenziali?</translation>
+    </message>
+    <message>
+        <source>Sign Out</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Change Server</source>
+        <translation>Cambia server</translation>
+    </message>
 </context>
 </TS>
