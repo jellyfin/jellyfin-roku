9f4ea2d1be0dfde32ec1a8561687947fa5d0bc6a
candry7731 <83685828+candry7731@users.noreply.github.com>
2022-06-11 19:15:47 -0500

    update translation and setting title

--- locale/en_US/translations.ts
+++ locale/en_US/translations.ts
@@ -573,5 +573,15 @@
         <source>There was an error authenticating via Quick Connect.</source>
         <translation>There was an error authenticating via Quick Connect.</translation>
     </message>
+    <message>
+    <source>Return to Top</source>
+    <translation>Return to Top</translation>
+    <extracomment>UI -> Media Grid -> Item Title in user setting screen.</extracomment>
+</message>
+<message>
+    <source>Use the replay button to slowly animate to the first item in the folder. (If disabled, The folder will reset to the first item immediately)</source>
+    <translation>Use the replay button to slowly animate to the first item in the folder. (If disabled, The folder will reset to the first item immediately)</translation>
+    <extracomment>Description for option in Setting Screen</extracomment>
+</message>
 </context>
 </TS>
