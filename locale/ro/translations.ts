4fd00fab4fb5452fd2ee4110e372d5c052ad3470
Emanuel Șipoș <emanuelsipos1@gmail.com>
2022-08-01 15:45:58 +0000

    Translated using Weblate (Romanian)
    Currently translated at 67.5% (102 of 151 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/ro/

--- locale/ro/translations.ts
+++ locale/ro/translations.ts
@@ -1348,7 +1348,7 @@
         <translation>Caracteristici speciale</translation>
         <message>
             <source>Press &apos;OK&apos; to Close</source>
-            <translation>Press &apos;OK&apos; to Close</translation>
+            <translation>Apasă &apos;OK&apos; pentru a închide</translation>
         </message>
     </message>
     <message>
@@ -1449,6 +1449,402 @@
         <source>There was an error retrieving the data for this item from the server.</source>
         <translation>S-a produs o eroare la preluarea datelor pentru acest articol de pe server.</translation>
         <extracomment>Dialog detail when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>Cinema mode</source>
+        <translation>Mod cinema</translation>
+        <extracomment>Settings Menu - Title for option</extracomment>
+    </message>
+    <message>
+        <source>Options that alter the design of Jellyfin.</source>
+        <translation type="unfinished">Opțiuni ce modifică designul Jellyfin.</translation>
+        <extracomment>Description for Design Elements user settings.</extracomment>
+    </message>
+    <message>
+        <source>Design Elements</source>
+        <translation>Elemente design</translation>
+    </message>
+    <message>
+        <source>If enabled, images for unwatched episodes will be blurred.</source>
+        <translation type="unfinished">Dacă pornit, imaginile pentru episoadele nevizualizate vor fi încețoșate.</translation>
+    </message>
+    <message>
+        <source>Blur Unwatched Episodes</source>
+        <translation type="unfinished">Încețoșare episoade nevizualizate</translation>
+        <extracomment>Option Title in user setting screen</extracomment>
+    </message>
+    <message>
+        <source>Options for TV Shows.</source>
+        <translation type="unfinished">Opțiuni pentru seriale TV.</translation>
+        <extracomment>Description for TV Shows user settings.</extracomment>
+    </message>
+    <message>
+        <source>Options for details pages.</source>
+        <translation>Opțiuni pentru pagina de detalii.</translation>
+        <extracomment>Description for Detail Page user settings.</extracomment>
+    </message>
+    <message>
+        <source>Detail Page</source>
+        <translation>Pagină detalii</translation>
+    </message>
+    <message>
+        <source>Return to Top</source>
+        <translation type="unfinished">Întoarce-te sus</translation>
+        <extracomment>UI -&gt; Media Grid -&gt; Item Title in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Shows</source>
+        <translation type="unfinished">Seriale</translation>
+    </message>
+    <message>
+        <source>Studios</source>
+        <translation type="unfinished">Studiouri</translation>
+    </message>
+    <message>
+        <source>Networks</source>
+        <translation>Rețele</translation>
+    </message>
+    <message>
+        <source>There was an error authenticating via Quick Connect.</source>
+        <translation>S-a întâmpinat o eroare în timpul Conectării Rapide.</translation>
+    </message>
+    <message>
+        <source>(Dialog will close automatically)</source>
+        <translation type="unfinished">(Dialogul se va închide automat)</translation>
+    </message>
+    <message>
+        <source>Quick Connect</source>
+        <translation>Conectare Rapidă</translation>
+    </message>
+    <message>
+        <source>Here is your Quick Connect code:</source>
+        <translation type="unfinished">Acesta este codul dumneavoastră de Conectare Rapidă:</translation>
+    </message>
+    <message>
+        <source>%1 of %2</source>
+        <translation>%1 din %2</translation>
+        <extracomment>Item position and count.  %1 = current item.  %2 = total number of items</extracomment>
+    </message>
+    <message>
+        <source>Go to episode</source>
+        <translation>Du-te la episod</translation>
+        <extracomment>Continue Watching Popup Menu - Navigate to the Episode Detail Page</extracomment>
+    </message>
+    <message>
+        <source>Go to season</source>
+        <translation>Du-te la sezon</translation>
+        <extracomment>Continue Watching Popup Menu - Navigate to the Season Page</extracomment>
+    </message>
+    <message>
+        <source>Go to series</source>
+        <translation type="unfinished">Du-te la serial</translation>
+        <extracomment>Continue Watching Popup Menu - Navigate to the Series Detail Page</extracomment>
+    </message>
+    <message>
+        <source>Set Watched</source>
+        <translation>Setare ca vizionat</translation>
+        <extracomment>Button Text - When pressed, marks item as Warched</extracomment>
+    </message>
+    <message>
+        <source>Set Favorite</source>
+        <translation>Setare ca favorit</translation>
+        <extracomment>Button Text - When pressed, sets item as Favorite</extracomment>
+    </message>
+    <message>
+        <source>Show item count in the library, and index of selected item.</source>
+        <translation type="unfinished">Arată numărul de elemente din bibliotecă, și indexul elementului selectat.</translation>
+        <extracomment>Description for option in Setting Screen</extracomment>
+    </message>
+    <message>
+        <source>Item Count</source>
+        <translation type="unfinished">Număr elemente</translation>
+        <extracomment>UI -&gt; Media Grid -&gt; Item Count in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Always show the titles below the poster images. (If disabled, title will be shown under hilighted item only)</source>
+        <translation type="unfinished">Arătați în totdeauna titlurile sub imaginile poster. (Dacă este oprit, titlurile vor fi arătate doar pentru conținutul selectat)</translation>
+        <extracomment>Description for option in Setting Screen</extracomment>
+    </message>
+    <message>
+        <source>Item Titles</source>
+        <translation type="unfinished">Titluri conținut</translation>
+        <extracomment>UI -&gt; Media Grid -&gt; Item Title in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>User Interface</source>
+        <translation>Interfață Utilizator</translation>
+        <extracomment>Title for User Interface section in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation type="unfinished">Oprit</translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation type="unfinished">Pornit</translation>
+    </message>
+    <message>
+        <source>Support direct play of MPEG 2 content (e.g. Live TV). This will prevent transcoding of MPEG 2 content, but uses significantly more bandwidth</source>
+        <translation type="unfinished">Suport pentru redarea directă a conținutului MPEG 2 (ex. Televiziune în direct). Conținutul MPEG 2 nu va fi transcodat, dar se va folosi significativ mai multă lățime de bandă.</translation>
+        <extracomment>Settings Menu - Description for option</extracomment>
+    </message>
+    <message>
+        <source>MPEG 2 Support</source>
+        <translation>Suport MPEG 2</translation>
+        <extracomment>Settings Menu - Title for option</extracomment>
+    </message>
+    <message>
+        <source>Playback</source>
+        <translation>Redare conținut</translation>
+        <extracomment>Title for Playback section in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versiune</translation>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item.  Server did not provide required transcoding data.</source>
+        <translation type="unfinished">S-a întâmpinat o eroare in timpul redării conținutului selectat.  Serverul nu a trimis datele necesare pentru transcodare.</translation>
+        <extracomment>Content of message box when trying to play an item which requires transcoding, and the server did not provide transcode url</extracomment>
+    </message>
+    <message>
+        <source>Error Getting Playback Information</source>
+        <translation type="unfinished">Eroare primire informație despre conținutul selectat</translation>
+        <extracomment>Dialog Title: Received error from server when trying to get information about the selected item for playback</extracomment>
+    </message>
+    <message>
+        <source>...or enter server URL manually:</source>
+        <translation type="unfinished">…sau adăugați URL-ul serverului manual:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to manually enter a server URL</extracomment>
+    </message>
+    <message>
+        <source>Pick a Jellyfin server from the local network</source>
+        <translation>Selectați un server Jellyfin din rețeaua locală</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to pick a server from a list</extracomment>
+    </message>
+    <message>
+        <source>Enter the server name or ip address</source>
+        <translation type="unfinished">Introduceți numele sau adresa ip a serverului</translation>
+        <extracomment>Title of KeyboardDialog when manually entering a server URL</extracomment>
+    </message>
+    <message>
+        <source>The requested content does not exist on the server</source>
+        <translation>Conținutul cerut nu există pe server</translation>
+        <extracomment>Content of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Necunoscut</translation>
+        <extracomment>Title for a cast member for which we have no information for</extracomment>
+    </message>
+    <message>
+        <source>Not found</source>
+        <translation>Nu a fost găsit</translation>
+        <extracomment>Title of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Connecting to Server</source>
+        <translation type="unfinished">Se conectează la server</translation>
+        <extracomment>Message to display to user while client is attempting to connect to the server</extracomment>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Închide</translation>
+    </message>
+    <message>
+        <source>Cancel Series Recording</source>
+        <translation>Anulează înregistrare serial</translation>
+    </message>
+    <message>
+        <source>Cancel Recording</source>
+        <translation>Anulează înregistrare</translation>
+    </message>
+    <message>
+        <source>Record Series</source>
+        <translation>Înregistrează serial</translation>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation>Înregistrează</translation>
+    </message>
+    <message>
+        <source>View Channel</source>
+        <translation>Vezi canal</translation>
+    </message>
+    <message>
+        <source>TV Guide</source>
+        <translation>Ghid TV</translation>
+        <extracomment>Menu option for showing Live TV Guide / Schedule</extracomment>
+    </message>
+    <message>
+        <source>Channels</source>
+        <translation>Canale</translation>
+        <extracomment>Menu option for showing Live TV Channel List</extracomment>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation type="unfinished">Redifuzare</translation>
+        <extracomment>If TV Shows has previously been broadcasted</extracomment>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>În direct</translation>
+        <extracomment>If TV Show is being broadcast live (not pre-recorded)</extracomment>
+    </message>
+    <message>
+        <source>Ends at</source>
+        <translation>Se termină</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program ended  (e.g.  Ended Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ended at</source>
+        <translation>S-a terminat la</translation>
+        <extracomment>(Past Tense) For defining time when a program will ended (e.g.  Ended at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts</source>
+        <translation>Începe</translation>
+        <extracomment>(Future Tense) For defining a day and time when a program will start (e.g.  Starts Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts at</source>
+        <translation>Începe la</translation>
+        <extracomment>(Future Tense) For defining time when a program will start today (e.g.  Starts at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Started</source>
+        <translation>A început</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program started (e.g.  Started Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Started at</source>
+        <translation>A început la</translation>
+        <extracomment>(Past Tense) For defining time when a program started today (e.g.  Started at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation>Sâmbătă</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation>Vineri</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation>Joi</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation>Miercuri</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation>Marți</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation>Luni</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation>Duminică</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation>mâine</translation>
+        <extracomment>Next day</extracomment>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation>ieri</translation>
+        <extracomment>Previous day</extracomment>
+    </message>
+    <message>
+        <source>today</source>
+        <translation>astăzi</translation>
+        <extracomment>Current day</extracomment>
+    </message>
+    <message>
+        <source>TV Shows</source>
+        <translation>Seriale TV</translation>
+    </message>
+    <message>
+        <source>Movies</source>
+        <translation>Filme</translation>
+    </message>
+    <message>
+        <source>More Like This</source>
+        <translation>Mai multe ca aceasta</translation>
+    </message>
+    <message>
+        <source>Age</source>
+        <translation>Vârstă</translation>
+    </message>
+    <message>
+        <source>Died</source>
+        <translation>Decedat</translation>
+    </message>
+    <message>
+        <source>Born</source>
+        <translation>Născut</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to filter library content</comment>
+        <source>TAB_FILTER</source>
+        <translation>Filtru</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to sort library content</comment>
+        <source>TAB_SORT</source>
+        <translation>Sortare</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for switching &quot;views&quot; when looking at a library</comment>
+        <source>TAB_VIEW</source>
+        <translation type="unfinished">Vedere</translation>
+    </message>
+    <message>
+        <source>RUNTIME</source>
+        <translation>Durată</translation>
+    </message>
+    <message>
+        <source>PLAY_COUNT</source>
+        <translation>Număr redări</translation>
+    </message>
+    <message>
+        <source>DATE_PLAYED</source>
+        <translation>Dată redare</translation>
+    </message>
+    <message>
+        <source>DATE_ADDED</source>
+        <translation>Dată adaugare</translation>
+    </message>
+    <message>
+        <source>CRITIC_RATING</source>
+        <translation>Scor critici</translation>
+    </message>
+    <message>
+        <source>On Now</source>
+        <translation>Acum</translation>
+    </message>
+    <message>
+        <source>Save Credentials?</source>
+        <translation>Salvezi datele de autentificare?</translation>
+    </message>
+    <message>
+        <source>IMDB_RATING</source>
+        <translation>Scor IMDb</translation>
+    </message>
+    <message>
+        <source>Delete Saved</source>
+        <translation>Șterge Salvate</translation>
     </message>
 </context>
 </TS>
