d8a2e2b5bae00b45d1e5860559734dce384cc07c
Csaba <csab0825@gmail.com>
2023-01-09 09:32:13 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 56.7% (131 of 231 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/hu/

--- locale/hu/translations.ts
+++ locale/hu/translations.ts
@@ -9318,5 +9318,425 @@ elemeket</translation>
         <source>Disable Community Rating for Episodes</source>
         <translation>Az epizódok közösségi értékelésének letiltása</translation>
     </message>
+    <message>
+        <source>PLAY_COUNT</source>
+        <translation>Lejátszások Száma</translation>
+    </message>
+    <message>
+        <source>More Like This</source>
+        <translation>Több hasonló</translation>
+    </message>
+    <message>
+        <source>Age</source>
+        <translation>Kor</translation>
+    </message>
+    <message>
+        <source>Save Credentials?</source>
+        <translation>Mented a hitelesítő adatokat?</translation>
+    </message>
+    <message>
+        <source>Set Favorite</source>
+        <translation>Kedvenc hozzáadása</translation>
+        <extracomment>Button Text - When pressed, sets item as Favorite</extracomment>
+    </message>
+    <message>
+        <comment>Title of Tab for switching &quot;views&quot; when looking at a library</comment>
+        <source>TAB_VIEW</source>
+        <translation>Nézet</translation>
+    </message>
+    <message>
+        <source>Press &apos;OK&apos; to Close</source>
+        <translation>Nyomd meg az „OK” gombot a bezáráshoz</translation>
+    </message>
+    <message>
+        <source>Cancel Recording</source>
+        <translation>Felvétel megállítása</translation>
+    </message>
+    <message>
+        <source>Pick a Jellyfin server from the local network</source>
+        <translation>Válassz ki egy Jellyfin szervert a helyi hálózatról:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to pick a server from a list</extracomment>
+    </message>
+    <message>
+        <source>Media Grid options.</source>
+        <translation>Média rács beállítások.</translation>
+    </message>
+    <message>
+        <source>Error During Playback</source>
+        <translation>Hiba történt a lejátszás közben</translation>
+        <extracomment>Dialog title when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item.</source>
+        <translation>Hiba történt az elem lejátszása közben.</translation>
+        <extracomment>Dialog detail when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>Unable to load Channel Data from the server</source>
+        <translation>Nem lehet betölteni a csatornaadatokat a szerverről</translation>
+    </message>
+    <message>
+        <source>OFFICIAL_RATING</source>
+        <translation>Korhatár</translation>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation>Hétfő</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Starts at</source>
+        <translation>Kezdődik majd</translation>
+        <extracomment>(Future Tense) For defining time when a program will start today (e.g. Starts at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ends at</source>
+        <translation>Vége volt</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program ended (e.g. Ended Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Enter the server name or IP address</source>
+        <translation>Add meg a szerver nevét vagy IP-címét</translation>
+        <extracomment>Title of KeyboardDialog when manually entering a server URL</extracomment>
+    </message>
+    <message>
+        <source>...or enter server URL manually:</source>
+        <translation>…vagy add meg kézzel a szerver URL-jét:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to manually enter a server URL</extracomment>
+    </message>
+    <message>
+        <source>An error was encountered while playing this item. Server did not provide required transcoding data.</source>
+        <translation>Hiba történt az elem lejátszása közben. A szerver nem biztosította a szükséges transzkódolási adatokat.</translation>
+        <extracomment>Content of message box when trying to play an item which requires transcoding, and the server did not provide transcode url</extracomment>
+    </message>
+    <message>
+        <source>Playback</source>
+        <translation>Lejátszás</translation>
+        <extracomment>Title for Playback section in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Enable or disable Direct Play for optional codecs</source>
+        <translation>A Direct Play engedélyezése vagy letiltása az opcionális kodekekhez</translation>
+        <extracomment>Settings Menu - Title for settings group related to codec support</extracomment>
+    </message>
+    <message>
+        <source>Support Direct Play of MPEG-2 content (e.g., Live TV). This will prevent transcoding of MPEG-2 content, but uses significantly more bandwidth.</source>
+        <translation>Támogatja az MPEG-2 tartalom közvetlen lejátszását (például élő tévéadás). Ez megakadályozza az MPEG-2 tartalom átkódolását, de lényegesen nagyobb sávszélességet használ.</translation>
+        <extracomment>Settings Menu - Description for option</extracomment>
+    </message>
+    <message>
+        <source>** EXPERIMENTAL** Support Direct Play of AV1 content if this Roku device supports it.</source>
+        <translation>**KÍSÉRLETI** Az AV1 tartalom közvetlen lejátszásának támogatása, ha ez a Roku eszköz támogatja.</translation>
+        <extracomment>Description of a setting - should we try to direct play experimental av1 codec</extracomment>
+    </message>
+    <message>
+        <source>Always show the titles below the poster images. (If disabled, the title will be shown under the highlighted item only).</source>
+        <translation>Mindig jelenítse meg a címeket a poszterképek alatt. (Ha le van tiltva, a cím csak a kiemelt elem alatt jelenik meg).</translation>
+        <extracomment>Description for option in Setting Screen</extracomment>
+    </message>
+    <message>
+        <source>Error loading Channel Data</source>
+        <translation>Hiba történt a csatornaadatok betöltésekor</translation>
+    </message>
+    <message>
+        <source>CRITIC_RATING</source>
+        <translation>Kritikusok Értékelése</translation>
+    </message>
+    <message>
+        <source>Died</source>
+        <translation>Elhunyt</translation>
+    </message>
+    <message>
+        <source>Error Retrieving Content</source>
+        <translation>Hiba a tartalom lekérésekor</translation>
+        <extracomment>Dialog title when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>Loading Channel Data</source>
+        <translation>Csatornaadatok betöltése</translation>
+    </message>
+    <message>
+        <comment>Name or Title field of media item</comment>
+        <source>TITLE</source>
+        <translation>Név</translation>
+    </message>
+    <message>
+        <source>RUNTIME</source>
+        <translation>Játékidő</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to filter library content</comment>
+        <source>TAB_FILTER</source>
+        <translation>Szűrő</translation>
+    </message>
+    <message>
+        <source>IMDB_RATING</source>
+        <translation>IMDb Értékelés</translation>
+    </message>
+    <message>
+        <source>DATE_ADDED</source>
+        <translation>Hozzáadva</translation>
+    </message>
+    <message>
+        <source>RELEASE_DATE</source>
+        <translation>Megjelenés dátuma</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to sort library content</comment>
+        <source>TAB_SORT</source>
+        <translation>Rendezés</translation>
+    </message>
+    <message>
+        <source>Movies (Grid)</source>
+        <translation>Filmek (Rács)</translation>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation>Szerda</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Started</source>
+        <translation>Kezdődött</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program started (e.g. Started Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>There was an error retrieving the data for this item from the server.</source>
+        <translation>Hiba történt az elem(ek) betöltése során a szerverről.</translation>
+        <extracomment>Dialog detail when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <comment>Message displayed in Item Grid when no item to display. %1 is container type (e.g. Boxset, Collection, Folder, etc)</comment>
+        <source>NO_ITEMS</source>
+        <translation>Ez a(z) %1 nem tartalmaz elemeket</translation>
+    </message>
+    <message>
+        <source>DATE_PLAYED</source>
+        <translation>Utoljára lejátszva</translation>
+    </message>
+    <message>
+        <source>Born</source>
+        <translation>Született</translation>
+    </message>
+    <message>
+        <source>Cast &amp; Crew</source>
+        <translation>Szereplők &amp; Stáb</translation>
+    </message>
+    <message>
+        <source>Additional Parts</source>
+        <translation>További részek</translation>
+        <extracomment>Additional parts of a video</extracomment>
+    </message>
+    <message>
+        <source>Support Direct Play of MPEG-4 content. This may need to be disabled for playback of DIVX encoded video files.</source>
+        <translation>Támogatja az MPEG-4 tartalom közvetlen lejátszását. Lehet, hogy ezt ki kell kapcsolni a DIVX kódolású videofájlok lejátszásához.</translation>
+        <extracomment>Settings Menu - Description for option</extracomment>
+    </message>
+    <message>
+        <source>Item Titles</source>
+        <translation>Fájl Címek</translation>
+        <extracomment>UI -&gt; Media Grid -&gt; Item Title in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Item Count</source>
+        <translation>Elemek száma</translation>
+        <extracomment>UI -&gt; Media Grid -&gt; Item Count in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Show item count in the library and index of selected item.</source>
+        <translation>Elemszám megjelenítése a könyvtárban és a kiválasztott elem indexében.</translation>
+        <extracomment>Description for option in Setting Screen</extracomment>
+    </message>
+    <message>
+        <source>Delete Saved</source>
+        <translation>Mentettek Törlése</translation>
+    </message>
+    <message>
+        <source>On Now</source>
+        <translation>Most</translation>
+    </message>
+    <message>
+        <source>Special Features</source>
+        <translation>Sajátosságok</translation>
+    </message>
+    <message>
+        <source>TV Shows</source>
+        <translation>TV Sorozatok</translation>
+    </message>
+    <message>
+        <source>today</source>
+        <translation>ma</translation>
+        <extracomment>Current day</extracomment>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation>Vasárnap</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation>Kedd</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation>Csütörtök</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation>Szombat</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Started at</source>
+        <translation>Ekkor kezdődött</translation>
+        <extracomment>(Past Tense) For defining time when a program started today (e.g. Started at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts</source>
+        <translation>Kezdődni fog</translation>
+        <extracomment>(Future Tense) For defining a day and time when a program will start (e.g. Starts Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ended at</source>
+        <translation>Vége lett</translation>
+        <extracomment>(Past Tense) For defining time when a program will ended (e.g. Ended at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>Élő</translation>
+        <extracomment>If TV Show is being broadcast live (not pre-recorded)</extracomment>
+    </message>
+    <message>
+        <source>TV Guide</source>
+        <translation>Műsorújság</translation>
+        <extracomment>Menu option for showing Live TV Guide / Schedule</extracomment>
+    </message>
+    <message>
+        <source>Record Series</source>
+        <translation>Sorozat felvétele</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Bezárás</translation>
+    </message>
+    <message>
+        <source>Error Getting Playback Information</source>
+        <translation>Hiba a lejátszási információk lekérésekor</translation>
+        <extracomment>Dialog Title: Received error from server when trying to get information about the selected item for playback</extracomment>
+    </message>
+    <message>
+        <source>Movies (Presentation)</source>
+        <translation>Filmek (bemutató)</translation>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation>Péntek</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Cancel Series Recording</source>
+        <translation>Sorozatfelvétel leállítása</translation>
+    </message>
+    <message>
+        <source>Connecting to Server</source>
+        <translation>Csatlakozás a szerverhez</translation>
+        <extracomment>Message to display to user while client is attempting to connect to the server</extracomment>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Verzió</translation>
+    </message>
+    <message>
+        <source>Media Grid</source>
+        <translation>Média Rács</translation>
+        <extracomment>UI -&gt; Media Grid section in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Codec Support</source>
+        <translation>Kodek támogatás</translation>
+        <extracomment>Settings Menu - Title for settings group related to codec support</extracomment>
+    </message>
+    <message>
+        <source>MPEG-2</source>
+        <translation>MPEG-2</translation>
+        <extracomment>Name of codec used in settings menu</extracomment>
+    </message>
+    <message>
+        <source>Set Watched</source>
+        <translation>Megnézve</translation>
+        <extracomment>Button Text - When pressed, marks item as Warched</extracomment>
+    </message>
+    <message>
+        <source>MPEG-4</source>
+        <translation>MPEG-4</translation>
+        <extracomment>Name of codec used in settings menu</extracomment>
+    </message>
+    <message>
+        <source>AV1</source>
+        <translation>AV1</translation>
+        <extracomment>Name of a setting - should we try to direct play experimental av1 codec</extracomment>
+    </message>
+    <message>
+        <source>User Interface</source>
+        <translation>Felhasználói felület</translation>
+        <extracomment>Title for User Interface section in user setting screen.</extracomment>
+    </message>
+    <message>
+        <source>Movies</source>
+        <translation>Filmek</translation>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation>tegnap</translation>
+        <extracomment>Previous day</extracomment>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation>holnap</translation>
+        <extracomment>Next day</extracomment>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation>Ismétlés</translation>
+        <extracomment>If TV Shows has previously been broadcasted</extracomment>
+    </message>
+    <message>
+        <source>Channels</source>
+        <translation>Csatornák</translation>
+        <extracomment>Menu option for showing Live TV Channel List</extracomment>
+    </message>
+    <message>
+        <source>View Channel</source>
+        <translation>Csatorna Megtekintése</translation>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation>Felvétel</translation>
+    </message>
+    <message>
+        <source>Not found</source>
+        <translation>Nem található</translation>
+        <extracomment>Title of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Ismeretlen</translation>
+        <extracomment>Title for a cast member for which we have no information for</extracomment>
+    </message>
+    <message>
+        <source>The requested content does not exist on the server</source>
+        <translation>A kért tartalom nem található a szerveren</translation>
+        <extracomment>Content of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation>Engedélyezve</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Kikapcsolva</translation>
+    </message>
 </context>
 </TS>
