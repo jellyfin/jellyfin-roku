9a598471a0a0c5d9341644a369f516b3b2086596
David Poznański <david@davidgameco.com>
2023-12-17 11:12:47 +0000

    Translated using Weblate (Italian)
    Currently translated at 35.1% (97 of 276 strings)
    
    Translation: Jellyfin/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin/jellyfin-roku/it/

--- locale/it_IT/translations.ts
+++ locale/it_IT/translations.ts
@@ -142,7 +142,7 @@
     </message>
     <message>
         <source>Sort Order</source>
-        <translation>Tiipo di ordinamento</translation>
+        <translation>Tipo di ordinamento</translation>
     </message>
     <message>
         <source>Descending</source>
@@ -2592,6 +2592,274 @@
     <message>
         <source>Save Credentials?</source>
         <translation>Salvare le credenziali?</translation>
+    </message>
+    <message>
+        <source>Change Server</source>
+        <translation>Cambia Server</translation>
+    </message>
+    <message>
+        <source>Sign Out</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Save Credentials?</source>
+        <translation>Salva credenziali?</translation>
+    </message>
+    <message>
+        <source>Delete Saved</source>
+        <translation>Cancella salvati</translation>
+    </message>
+    <message>
+        <source>Error During Playback</source>
+        <translation>Errore durante riproduzione</translation>
+        <extracomment>Dialog title when error occurs during playback</extracomment>
+    </message>
+    <message>
+        <source>RUNTIME</source>
+        <translation>Durata</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to filter library content</comment>
+        <source>TAB_FILTER</source>
+        <translation>Filtra</translation>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation>ieri</translation>
+        <extracomment>Previous day</extracomment>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation>Martedì</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation>Mercoledì</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation>Giovedì</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Started</source>
+        <translation>Iniziato</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program started (e.g. Started Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Starts at</source>
+        <translation>Inizia a</translation>
+        <extracomment>(Future Tense) For defining time when a program will start today (e.g. Starts at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ended at</source>
+        <translation>Terminato alle</translation>
+        <extracomment>(Past Tense) For defining time when a program will ended (e.g. Ended at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Ends at</source>
+        <translation>Termina</translation>
+        <extracomment>(Past Tense) For defining a day and time when a program ended (e.g. Ended Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Error Getting Playback Information</source>
+        <translation>Errore nel recupero delle informazioni di riproduzione</translation>
+        <extracomment>Dialog Title: Received error from server when trying to get information about the selected item for playback</extracomment>
+    </message>
+    <message>
+        <source>View Channel</source>
+        <translation>Guarda il Canale</translation>
+    </message>
+    <message>
+        <source>Died</source>
+        <translation>Morto</translation>
+    </message>
+    <message>
+        <source>Cast &amp; Crew</source>
+        <translation>Cast &amp; Troupe</translation>
+    </message>
+    <message>
+        <source>today</source>
+        <translation>oggi</translation>
+        <extracomment>Current day</extracomment>
+    </message>
+    <message>
+        <source>Age</source>
+        <translation>Età</translation>
+    </message>
+    <message>
+        <source>IMDB_RATING</source>
+        <translation>Valutazione di IMDb</translation>
+    </message>
+    <message>
+        <comment>Name or Title field of media item</comment>
+        <source>TITLE</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for options to sort library content</comment>
+        <source>TAB_SORT</source>
+        <translation>Ordina</translation>
+    </message>
+    <message>
+        <comment>Title of Tab for switching &quot;views&quot; when looking at a library</comment>
+        <source>TAB_VIEW</source>
+        <translation>Visualizzazione</translation>
+    </message>
+    <message>
+        <source>Starts</source>
+        <translation>Inizia</translation>
+        <extracomment>(Future Tense) For defining a day and time when a program will start (e.g. Starts Wednesday, 08:00) </extracomment>
+    </message>
+    <message>
+        <source>More Like This</source>
+        <translation>Simile a questo</translation>
+    </message>
+    <message>
+        <source>Record Series</source>
+        <translation>Registra Serie</translation>
+    </message>
+    <message>
+        <source>On Now</source>
+        <translation>In onda ora</translation>
+    </message>
+    <message>
+        <source>There was an error retrieving the data for this item from the server.</source>
+        <translation>C&apos;è stato un errore nel recupero dei dati per questo elemento dal server.</translation>
+        <extracomment>Dialog detail when unable to load Content from Server</extracomment>
+    </message>
+    <message>
+        <source>CRITIC_RATING</source>
+        <translation>Valutazione della critica</translation>
+    </message>
+    <message>
+        <source>DATE_ADDED</source>
+        <translation>Data di aggiunta</translation>
+    </message>
+    <message>
+        <source>DATE_PLAYED</source>
+        <translation>Visto il</translation>
+    </message>
+    <message>
+        <source>OFFICIAL_RATING</source>
+        <translation>Classificazione Parentale</translation>
+    </message>
+    <message>
+        <source>PLAY_COUNT</source>
+        <translation>Numero di Riproduzioni</translation>
+    </message>
+    <message>
+        <source>Born</source>
+        <translation>Nato</translation>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation>Domenica</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation>Lunedì</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation>Sabato</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation>Venerdì</translation>
+        <extracomment>Day of Week</extracomment>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>In Diretta</translation>
+        <extracomment>If TV Show is being broadcast live (not pre-recorded)</extracomment>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation>Replica</translation>
+        <extracomment>If TV Shows has previously been broadcasted</extracomment>
+    </message>
+    <message>
+        <source>TV Guide</source>
+        <translation>Guida TV</translation>
+        <extracomment>Menu option for showing Live TV Guide / Schedule</extracomment>
+    </message>
+    <message>
+        <source>Channels</source>
+        <translation>Canali</translation>
+        <extracomment>Menu option for showing Live TV Channel List</extracomment>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation>Registra</translation>
+    </message>
+    <message>
+        <source>Cancel Recording</source>
+        <translation>Annulla Registrazione</translation>
+    </message>
+    <message>
+        <source>Special Features</source>
+        <translation>Contenuti Speciali</translation>
+    </message>
+    <message>
+        <source>Movies</source>
+        <translation>Film</translation>
+    </message>
+    <message>
+        <source>RELEASE_DATE</source>
+        <translation>Data di Rilascio</translation>
+    </message>
+    <message>
+        <source>TV Shows</source>
+        <translation>Serie TV</translation>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation>domani</translation>
+        <extracomment>Next day</extracomment>
+    </message>
+    <message>
+        <source>Started at</source>
+        <translation>Iniziato a</translation>
+        <extracomment>(Past Tense) For defining time when a program started today (e.g. Started at 08:00) </extracomment>
+    </message>
+    <message>
+        <source>Cancel Series Recording</source>
+        <translation>Annulla Registrazione della Serie</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <source>Connecting to Server</source>
+        <translation>Connessione al Server</translation>
+        <extracomment>Message to display to user while client is attempting to connect to the server</extracomment>
+    </message>
+    <message>
+        <source>Not found</source>
+        <translation>Non trovato</translation>
+        <extracomment>Title of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>The requested content does not exist on the server</source>
+        <translation>Il contenuto richiesto non esiste sul server</translation>
+        <extracomment>Content of message box when the requested content is not found on the server</extracomment>
+    </message>
+    <message>
+        <source>Pick a Jellyfin server from the local network</source>
+        <translation>Scegli un server Jellyfin dalla rete locale:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to pick a server from a list</extracomment>
+    </message>
+    <message>
+        <source>...or enter server URL manually:</source>
+        <translation>Se il server non è nella lista, puoi inserire l&apos;URL manualmente:</translation>
+        <extracomment>Instructions on initial app launch when the user is asked to manually enter a server URL</extracomment>
     </message>
     <message>
         <source>Change Server</source>
