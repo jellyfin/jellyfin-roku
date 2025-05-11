b27e75f6194d4c7638372842c168d9b9f9e363a4
myrad2267 <myrad2267@gmail.com>
2025-05-10 22:02:16 -0400

    Translated using Weblate (French)
    Currently translated at 100.0% (298 of 298 strings)
    
    Translation: Jellyfin Roku/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin-roku/jellyfin-roku/fr/

--- locale/fr/translations.ts
+++ locale/fr/translations.ts
@@ -1325,5 +1325,25 @@
         <source>The number of episodes that will play automatically after the selected episode. Requires &apos;Play next episode automatically&apos; server setting to be enabled.</source>
         <translation>Le nombre d&apos;épisodes qui seront lus automatiquement après l&apos;épisode sélectionné. Nécessite que le paramètre du serveur «&#xa0;Lire automatiquement l&apos;épisode suivant&#xa0;» soit activé.</translation>
     </message>
+    <message>
+        <source>Return to server select screen</source>
+        <translation>Retourner à l&apos;écran de sélection du serveur</translation>
+    </message>
+    <message>
+        <source>The selected Jellyfin server uses version</source>
+        <translation>Le serveur Jellyfin sélectionné utilise la version</translation>
+    </message>
+    <message>
+        <source>which is not supported by this app. Please update the server to</source>
+        <translation>qui n&apos;est pas pris en charge par cette application. Veuillez mettre à jour le serveur pour</translation>
+    </message>
+    <message>
+        <source>or newer to use this app or install and use the Jellyfin Legacy app from Roku&apos;s Streaming Store</source>
+        <translation>ou plus récent pour utiliser cette application ou installer et utiliser l&apos;application Jellyfin Legacy depuis le Streaming Store de Roku</translation>
+    </message>
+    <message>
+        <source>CH</source>
+        <translation>Ch</translation>
+    </message>
 </context>
 </TS>
