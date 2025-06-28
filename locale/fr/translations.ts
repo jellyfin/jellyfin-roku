3d657839cf2fad3415328e78047559decbff9074
myrad2267 <myrad2267@gmail.com>
2025-06-28 09:45:39 -0400

    Translated using Weblate (French)
    Currently translated at 100.0% (312 of 312 strings)
    
    Translation: Jellyfin Roku/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin-roku/jellyfin-roku/fr/

--- locale/fr/translations.ts
+++ locale/fr/translations.ts
@@ -1365,5 +1365,41 @@
         <source>Go To Artist</source>
         <translation>Afficher l&apos;artiste</translation>
     </message>
+    <message>
+        <source>downloads</source>
+        <translation>téléchargements</translation>
+    </message>
+    <message>
+        <source>Series</source>
+        <translation>Séries</translation>
+    </message>
+    <message>
+        <source>Skip</source>
+        <translation>Passer</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <source>No Results Found</source>
+        <translation>Aucun résultat trouvé</translation>
+    </message>
+    <message>
+        <source>What&apos;s New</source>
+        <translation>Quoi de neuf</translation>
+    </message>
+    <message>
+        <source>Play / Resume from</source>
+        <translation>Lire / Reprendre à partir de</translation>
+    </message>
+    <message>
+        <source>Play or Resume from</source>
+        <translation>Lire ou reprendre à partir de</translation>
+    </message>
+    <message>
+        <source>Part</source>
+        <translation>Partie</translation>
+    </message>
 </context>
 </TS>
