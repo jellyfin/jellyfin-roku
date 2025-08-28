b4940c08f3934d9c589b56ea72da630861cb39be
myrad2267 <37258280+myrad2267@users.noreply.github.com>
2025-08-28 06:37:38 -0400

    Translated using Weblate (French)
    Currently translated at 100.0% (479 of 479 strings)
    
    Translation: Jellyfin Roku/Jellyfin Roku
    Translate-URL: https://translate.jellyfin.org/projects/jellyfin-roku/jellyfin-roku/fr/

--- locale/fr/translations.ts
+++ locale/fr/translations.ts
@@ -2065,5 +2065,9 @@
         <source>Shuffle Play Collection</source>
         <translation>Lecture aléatoire de collection</translation>
     </message>
+    <message>
+        <source>Note: Force transcode option automatically enabled due to video rotation</source>
+        <translation>Remarque : l&apos;option de transcodage forcé est automatiquement activée en raison de la rotation de la vidéo</translation>
+    </message>
 </context>
 </TS>
